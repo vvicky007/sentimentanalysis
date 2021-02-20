e71ffe1d00365f42ec68ddaddabc8d6bf21071a6
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2021-02-20 18:52:10 +0530

    Add files via upload

---
+++ db/mongoose.js
@@ -0,0 +1,16 @@
+
+const url = `mongodb://127.0.0.1:27017/users`
+const mongoose = require('mongoose')
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    
+})
+const connection = mongoose.connection;
+
+connection.once("open", function() {
+  console.log("MongoDB database connection established successfully");
+ 
+});
+module.exports = mongoose
