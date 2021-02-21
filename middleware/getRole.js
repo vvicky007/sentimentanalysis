06c6b0324ccb80f4d802aa4ddc27bce8017e6223
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2021-02-21 12:43:21 +0530

    Update getRole.js

--- middleware/getRole.js
+++ middleware/getRole.js
@@ -1,33 +1,26 @@
-const jwt = require('jsonwebtoken')
-const User = require('../db/models/users')
-const {secret,customer,admin,superadmin} = require('../constants')
-exports.getRole= async(req, res, next) =>
-{   
-    if (!req.headers.authorization)
-        res.status(401).send({ message: "Not authorized to access data" });
-    else 
-    {  
-        const token = req.headers.authorization.split(" ")[1];
-        if (!token)
-            res.status(401).send({ message: "Not Authorized to access data" });
-        else 
-        {
-            try
-            {
-
-                const decoded = jwt.verify(token, secret)
-                const user = await User.findOne({ _id: decoded.id})
-                req.admin = decoded.role===admin
-                req.superadmin = decoded.role===superadmin
-                req.token = token
-                req.user = user
-                next()
-            
-            }
-            catch(e){
-                console.log(e)
-                res.status(401).send({ message:'Please Login again'})
-            }
-        }
+const jwt = require("jsonwebtoken");
+const User = require("../db/models/users");
+const { secret, customer, admin, superadmin } = require("../constants");
+exports.getRole = async (req, res, next) => {
+  if (!req.headers.authorization)
+    res.status(401).send({ message: "Not authorized to access data" });
+  else {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token)
+      res.status(401).send({ message: "Not Authorized to access data" });
+    else {
+      try {
+        const decoded = jwt.verify(token, secret);
+        const user = await User.findOne({ _id: decoded.id });
+        req.admin = decoded.role === admin;
+        req.superadmin = decoded.role === superadmin;
+        req.token = token;
+        req.user = user;
+        next();
+      } catch (e) {
+        console.log(e);
+        res.status(401).send({ message: "Please Login again" });
+      }
     }
-}
+  }
+};
