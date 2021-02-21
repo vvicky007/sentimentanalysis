806531871cd2ccbb4682c78c55baee749c36167b
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2021-02-21 13:53:34 +0530

    Update verifyToken.js

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,13 +1,16 @@
 const { secret } = require(".././constants");
 const jwt = require("jsonwebtoken");
 const User = require("../db/models/users");
+const { UNAUTHORIZED } = require("../constants");
 exports.verifyToken = async (req, res, next) => {
   if (!req.headers.authorization)
-    res.status(401).send({ message: "Not authorized to access data" });
+    res.status(UNAUTHORIZED).send({ message: "Not authorized to access data" });
   else {
     const token = req.headers.authorization.split(" ")[1];
     if (!token)
-      res.status(401).send({ message: "Not Authorized to access data" });
+      res
+        .status(UNAUTHORIZED)
+        .send({ message: "Not Authorized to access data" });
     else {
       try {
         const decoded = jwt.verify(token, secret);
@@ -16,7 +19,7 @@ exports.verifyToken = async (req, res, next) => {
         next();
       } catch (e) {
         console.log(e);
-        res.status(401).send({ message: "Please Login again" });
+        res.status(UNAUTHORIZED).send({ message: "Please Login again" });
       }
     }
   }
