03bc52ca60b4196c043caa49765588976491c80c
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2021-02-21 18:28:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const config = require("./config");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
