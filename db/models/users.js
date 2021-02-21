d2a6e3ad5a73f0c2407dc5faa68ac83f206edc70
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2021-02-21 12:41:26 +0530

    Add files via upload

--- db/models/users.js
+++ db/models/users.js
@@ -1,69 +1,72 @@
-const mongoose = require('../mongoose')
-const validator = require('validator')
-const {secret,customer,admin,superadmin} = require('../../constants')
-const jwt = require('jsonwebtoken')
+const mongoose = require("../mongoose");
+const validator = require("validator");
+const { secret, customer, admin, superadmin } = require("../../constants");
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-        validate(value){
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
     },
-    password:{
-        type:String,
-        required:true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
     },
-    role:{
-        type:String,
-        required:true
+  ],
+  posts: [
+    {
+      post: {
+        type: String,
+      },
     },
-    tokens: [{
-        token: {
-            type: String,
-            required: true
-        }
-    }],
-    posts: [{
-        post: {
-            type: String
-        }
-    }],
-    actions:[{
-        action:{
-            type:String
-        }
-    }
-    ]
-   
-})
-userSchema.methods.generateToken = async function() 
-{    
-    const user = this
-    let role = "Na" ;
-    if(user.role.toUpperCase() === customer){
-        role = customer
-    }
-    if(user.role.toUpperCase() === admin){
-        role = admin
-    }
-    if(user.role.toUpperCase()=== superadmin){
-        role= superadmin
-    }
-    
-    const token = await jwt.sign({role,id:user._id.toString()},secret,{expiresIn: '24h'})
-    user.tokens = user.tokens.concat({token})
-    return token
+  ],
+  actions: [
+    {
+      action: {
+        type: String,
+      },
+    },
+  ],
+});
+userSchema.methods.generateToken = async function () {
+  const user = this;
+  let role = "Na";
+  if (user.role.toUpperCase() === customer) {
+    role = customer;
+  }
+  if (user.role.toUpperCase() === admin) {
+    role = admin;
+  }
+  if (user.role.toUpperCase() === superadmin) {
+    role = superadmin;
+  }
 
+  const token = await jwt.sign({ role, id: user._id.toString() }, secret, {
+    expiresIn: "24h",
+  });
+  user.tokens = user.tokens.concat({ token });
+  return token;
+};
+userSchema.statics.findUser = async (email) => {
+  const user = await User.findOne({ email });
+  return user;
+};
+const User = mongoose.model("User", userSchema);
 
-}
-userSchema.statics.findUser = async (email)=>{
-    const user = await User.findOne({ email })
-    return user
-}
-const User = mongoose.model('User', userSchema)
-
-module.exports = User
+module.exports = User;
