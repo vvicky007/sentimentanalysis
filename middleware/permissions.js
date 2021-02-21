d32e7cf0ff4feeb0c4a2a1764dd4ebc4dffc11fb
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2021-02-21 12:43:49 +0530

    Update permissions.js

--- middleware/permissions.js
+++ middleware/permissions.js
@@ -1,24 +1,26 @@
-const User = require('../db/models/users')
-const axios = require('axios')
-exports.permissions= async(req, res, next) =>
-{
-    if(req.admin){
-        const {email} = req.body;
-        
-        try
+const User = require("../db/models/users");
+const axios = require("axios");
+exports.permissions = async (req, res, next) => {
+  if (req.admin) {
+    const { email } = req.body;
+
+    try {
+      const user = await User.findUser(email);
+      const resp = await axios.get(
+        "http://localhost:3000/superadmin/permissions/" + user.id,
         {
-            const user = await User.findUser(email)
-            const resp =await axios.get('http://localhost:3000/superadmin/permissions/'+user.id,{
-                headers: { "Content-Type": "application/json",Authorization: "Bearer " + req.token },
-            })
-            req.permissions = resp.data.permission
-            next()
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + req.token,
+          },
         }
-        catch(e){
-            res.status(500).json({message:e})
-        }
+      );
+      req.permissions = resp.data.permission;
+      next();
+    } catch (e) {
+      res.status(500).json({ message: e });
     }
-    else{
-        res.staus(401).json({message:'not an admin'})
-    }
-}
+  } else {
+    res.staus(401).json({ message: "not an admin" });
+  }
+};
