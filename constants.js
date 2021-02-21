09d28a052ed8988f6a6cdcfb379f8608da4f86fe
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2021-02-21 13:54:58 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -2,3 +2,7 @@ exports.secret = "Oslash";
 exports.admin = "ADMIN";
 exports.customer = "USER";
 exports.superadmin = "SUPERADMIN";
+exports.UNAUTHORIZED = 401;
+exports.NOTFOUND = 404;
+exports.SERVERERROR = 500;
+exports.OK = 200;
