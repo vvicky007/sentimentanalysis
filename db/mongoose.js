ca71188fe299ea1d7a4994e0417039c43c440689
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2021-02-21 12:42:37 +0530

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,16 +1,13 @@
-
-const url = `mongodb://127.0.0.1:27017/users`
-const mongoose = require('mongoose')
+const url = `mongodb://127.0.0.1:27017/users`;
+const mongoose = require("mongoose");
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    
-})
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 const connection = mongoose.connection;
 
-connection.once("open", function() {
+connection.once("open", function () {
   console.log("MongoDB database connection established successfully");
- 
 });
-module.exports = mongoose
+module.exports = mongoose;
